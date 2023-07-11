8081fa989aff5557c997b4054598f85fabcf3dac
Stefan Vukovic <stefan@Stefans-MacBook-Air.local>
2023-07-11 18:29:29 +0200

    authentication finished

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 app_name = 'core'
 urlpatterns = [
-    path('', views.index, name = 'index')
+    path('', views.index, name = 'index'),
+    # Authentication urls
+    path('login/', views.login_request, name = 'login'),
+    path('logout/', views.logout_request, name = 'logout'),
+    path('register/', views.register_request, name = 'register'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+# Authentication imports
+from django.contrib.auth import login, authenticate 
+from django.contrib.auth import logout
+from django.contrib.auth.forms import AuthenticationForm 
+from .forms import RegisterForm
+
 
 def index(request):
     return render(request, 'core/index.html')
+
+# Authentication functions start
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password =  request.POST.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('core:index')
+
+    form = AuthenticationForm()
+    context={"login_form":form}
+    return render(request, 'core/login.html', context)
+
+
+def logout_request(request):
+    logout(request)
+    return redirect('core:login')
+
+
+def register_request(request):
+    form = RegisterForm()
+    if request.method == "POST":
+        form = RegisterForm(request.POST)   
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+        return redirect("/")
+    else:
+        form = RegisterForm()
+
+    return render(request, "core/register.html", {"form":form})
+
+# Authentication functions end
